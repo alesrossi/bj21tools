390f875e9628ce47bb46633a196dc02fa55a6f8a
Alessandro <alessandro0024@gmail.com>
2025-01-04 22:27:15 +0000

    Added simple betting system

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ let newDeck: Deck;
 
 function App() {
   const [card, setCard] = useState<Card | null>(null);
+  const [betAmount, setBetAmount] = useState<number>(0);
 
   useEffect(() => {
     newDeck = new Deck();
@@ -22,6 +23,11 @@ function App() {
     }
   };
 
+  const raiseBet = (amount: number) => {
+    setBetAmount((prevBetAmount) => (prevBetAmount += amount));
+    console.log(betAmount);
+  };
+
   return (
     <>
       <div className="card">
@@ -34,6 +40,49 @@ function App() {
         )}
       </div>
       <button onClick={drawCard}>Draw Card</button>
+      <div className="fixed bottom-4 left-4 space-x-2">
+        <div className="vertical ">
+          <p className="left-1">BET: {betAmount ? betAmount : 0}</p>
+          <div className="space-x-2">
+            <button
+              onClick={() => raiseBet(1)}
+              className="p-2 bg-white text-black rounded"
+            >
+              1
+            </button>
+            <button
+              onClick={() => raiseBet(5)}
+              className="p-2 bg-red-500 text-white rounded"
+            >
+              5
+            </button>
+            <button
+              onClick={() => raiseBet(10)}
+              className="p-2 bg-blue-500 text-white rounded"
+            >
+              10
+            </button>
+            <button
+              onClick={() => raiseBet(25)}
+              className="p-2 bg-green-500 text-white rounded"
+            >
+              25
+            </button>
+            <button
+              onClick={() => raiseBet(100)}
+              className="p-2 bg-black text-white rounded"
+            >
+              100
+            </button>
+            <button
+              onClick={() => raiseBet(500)}
+              className="p-2 bg-purple-500 text-white rounded"
+            >
+              500
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
